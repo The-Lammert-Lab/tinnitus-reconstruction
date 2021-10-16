5dc6f5ceb6445735aceaa7a1526572dfa5040651
Alec Hoyland <alec.hoyland@posteo.net>
2021-10-16 12:20:20 -0400

    Make figures for Adam

--- code/analyze_pilot_data_from_adam.m
+++ code/analyze_pilot_data_from_adam.m
@@ -1,6 +1,6 @@
 %% Analyze Pilot Data from Adam
 
-RECOMPUTE = true;
+RECOMPUTE = false;
 
 if RECOMPUTE == true
 
@@ -21,19 +21,6 @@ if RECOMPUTE == true
     delete S;
     
     frequencies = 1:2:(2*length(R));
-    
-    % Get binned representation
-    % This is *not* a binary representation
-    % because the amplitude values are not binary
-    % (e.g., only -20 and 0).
-    n_bins = 32;
-    binned_repr = zeros(size(T, 1), n_bins);
-    for bin_num = 1:n_bins
-        a = T(:, B == bin_num);
-        binned_repr(:, bin_num) = a(:, 1);
-    end
-
-    return
 
     %% Compute reconstructions
     % varying the number of trials
@@ -61,7 +48,7 @@ end
 %% Plotting
 
 % figure with all the different trial lengths overlaid
-figure;
+figure('OuterPosition', [2 2 1500 1000], 'PaperUnits', 'points', 'PaperSize', [1000 1000], 'Name', 'Phi=spectra-0');
 n_plots = 3;%4;
 
 % create legend
@@ -93,12 +80,13 @@ legend(ax(3), leg)
 % ylabel('power (dB)')
 % xlabel(ax(4), 'frequency (kHz)')
 
+title('Phi = spectra')
 figlib.label()
 figlib.pretty()
 
 % generate a new figure for each trial length
 for ii = 1:length(n_trials)
-    figure;
+    figure('OuterPosition', [2 2 1500 1000], 'PaperUnits', 'points', 'PaperSize', [1000 1000], 'Name', ['Phi=spectra', '-', num2str(ii)]);
     n_plots = 3;%4;
 
     for qq = n_plots:-1:1
@@ -122,7 +110,7 @@ for ii = 1:length(n_trials)
     % ylabel('power (dB)')
     % xlabel(ax(4), 'frequency (kHz)')
 
-    title(['N = ' num2str(n_trials(ii))]);
+    title(['Phi = spectra, ', 'N = ' num2str(n_trials(ii))]);
     figlib.label()
     figlib.pretty()
 end
@@ -130,6 +118,11 @@ end
 %% Compute reconstructions
 % using the binned representation
 % varying the number of trials
+
+% get the binned representation
+% note that this is not a binary representation
+% since the amplitude is not just -20 and 0 dB
+binned_repr = spect2binnedrepr(T, B);
 
 n_trials = round([0.01, 0.03, 0.1, 0.3, 1] * length(I));
 x_gs = zeros(length(R), length(n_trials));
@@ -144,26 +137,21 @@ for ii = 1:length(n_trials)
     x_gs_binned(:, ii) = gs(this_I, this_T);
 
     % Compressed Sensing with Basis
-    x_cs_binned(:, ii) = cs(this_I, this_T, round(0.1 * size(T, 2)));
+    x_cs_binned(:, ii) = cs(this_I, this_T, 6);
 
     % Compressed Sensing no Basis
-    x_cs_nb_binned(:, ii) = cs_no_basis(this_I, this_T, round(0.1 * size(T, 2)));
+    x_cs_nb_binned(:, ii) = cs_no_basis(this_I, this_T, 6);
 end
 
-% resynthesize waveforms from binned representation
-x_gs_binned_spect = zeros(size(T));
-for ii = 1:size(x_gs_binned_spect, 1)
-    for qq = 1:n_bins
-        x_gs_binned_spect(ii, B == qq) = x_gs_binned(ii, qq);
-    end
-end
-
-
+% reconstruct the spectra from the binned representation
+x_gs_binned_spect = binnedrepr2spect(x_gs_binned', B)';
+x_cs_binned_spect = binnedrepr2spect(x_cs_binned', B)';
+x_cs_nb_binned_spect = binnedrepr2spect(x_cs_nb_binned', B)';
 
 %% Plotting (binned representation)
 
 % figure with all the different trial lengths overlaid
-figure;
+figure('OuterPosition', [2 2 1500 1000], 'PaperUnits', 'points', 'PaperSize', [1000 1000], 'Name', ['Phi=bins-0']);
 n_plots = 3;%4;
 
 % create legend
@@ -181,12 +169,12 @@ plot(ax(1), 1e-3 * frequencies, R);
 ylabel('power (dB)')
 
 % linear regression
-plot(ax(2), 1e-3 * frequencies, x_gs)
+plot(ax(2), 1e-3 * frequencies, x_gs_binned_spect)
 % ylabel('power (dB)')
 legend(ax(2), leg)
 
 % compressed sensing, with basis
-plot(ax(3), 1e-3 * frequencies, x_cs);
+plot(ax(3), 1e-3 * frequencies, x_cs_binned_spect);
 % ylabel('power (dB)')
 legend(ax(3), leg)
 
@@ -195,12 +183,13 @@ legend(ax(3), leg)
 % ylabel('power (dB)')
 % xlabel(ax(4), 'frequency (kHz)')
 
+title('Phi = binned repr')
 figlib.label()
 figlib.pretty()
 
 % generate a new figure for each trial length
 for ii = 1:length(n_trials)
-    figure;
+    figure('OuterPosition', [2 2 1500 1000], 'PaperUnits', 'points', 'PaperSize', [1000 1000], 'Name', ['Phi=bins', '-', num2str(ii)]);
     n_plots = 3;%4;
 
     for qq = n_plots:-1:1
@@ -212,11 +201,11 @@ for ii = 1:length(n_trials)
     ylabel('power (dB)')
 
     % linear regression
-    plot(ax(2), 1e-3 * frequencies, x_gs(:, ii))
+    plot(ax(2), 1e-3 * frequencies, x_gs_binned_spect(:, ii))
     % ylabel('power (dB)')
 
     % compressed sensing, with basis
-    plot(ax(3), 1e-3 * frequencies, x_cs(:, ii));
+    plot(ax(3), 1e-3 * frequencies, x_cs_binned_spect(:, ii));
     % ylabel('power (dB)')
 
     % % compressed sensing, no basis
@@ -224,7 +213,7 @@ for ii = 1:length(n_trials)
     % ylabel('power (dB)')
     % xlabel(ax(4), 'frequency (kHz)')
 
-    title(['N = ' num2str(n_trials(ii))]);
+    title(['Phi = binned repr, ', 'N = ' num2str(n_trials(ii))]);
     figlib.label()
     figlib.pretty()
 end
