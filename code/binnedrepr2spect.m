1f82139a22ada5ae7a8cef9a9d73e5b73435c1ca
Alec Hoyland <alec.hoyland@posteo.net>
2021-10-16 11:31:37 -0400

    Add functions to move between binned representation and spectra

---
+++ code/binnedrepr2spect.m
@@ -0,0 +1,33 @@
+function T = binnedrepr2spect(binned_repr, B, n_bins)
+    %   T = spect2binnedrepr(binned_repr, B)
+    %   T = spect2binnedrepr(binned_repr, B, n_bins)
+    %
+    % Get the stimuli spectra from a binned representation.
+    %
+    % ARGUMENTS:
+    % binned_repr: n_trials x n_bins matrix
+    %   representing the amplitude in each frequency bin
+    %   for each trial
+    % B: 1 x n_frequencies vector
+    %   representing the bin numbers
+    %   (e.g., [1, 1, 2, 2, 2, 3, 3, 3, 3, ...])
+    % n_bins: 1x1 scalar
+    %   representing the number of bins
+    %   if not passed as an argument,
+    %   it is computed from the maximum of B
+    % 
+    % OUTPUTS:
+    % T: n_trials x n_frequencies matrix
+    %   representing the stimulus spectra
+    % 
+    % See Also: binnedrepr2spect, spect2bin, bin2spect
+
+    if nargin < 3
+        n_bins = max(B);
+    end
+
+    T = zeros(size(binned_repr, 1), length(B));
+    for bin_num = 1:n_bins
+        T(:, B == bin_num) = repmat(binned_repr(:, bin_num), 1, sum(B == bin_num));
+    end
+end
---
+++ code/spect2binnedrepr.m
@@ -0,0 +1,36 @@
+function binned_repr = spect2binnedrepr(T, B, n_bins)
+    %   binned_repr = spect2binnedrepr(T, B)
+    %   binned_repr = spect2binnedrepr(T, B, n_bins)
+    %
+    % Get the binned representation,
+    % which is a vector containing the amplitude
+    % of the spectrum in each frequency bin.
+    %
+    % ARGUMENTS:
+    % T: n_trials x n_frequencies matrix
+    %   representing the stimulus spectra
+    % B: 1 x n_frequencies vector
+    %   representing the bin numbers
+    %   (e.g., [1, 1, 2, 2, 2, 3, 3, 3, 3, ...])
+    % n_bins: 1x1 scalar
+    %   representing the number of bins
+    %   if not passed as an argument,
+    %   it is computed from the maximum of B
+    % 
+    % OUTPUTS:
+    % binned_repr: n_trials x n_bins matrix
+    %   representing the amplitude for each frequency bin
+    %   for each trial
+    % 
+    % See Also: binnedrepr2spect, spect2bin, bin2spect
+
+    if nargin < 3
+        n_bins = max(B);
+    end
+
+    binned_repr = zeros(size(T, 1), n_bins);
+    for bin_num = 1:n_bins
+        a = T(:, B == bin_num);
+        binned_repr(:, bin_num) = a(:, 1);
+    end
+end
