e8822fc9cbf9122fe00bff9fb8260eca7fc2bd21
Alec Hoyland <alec.hoyland@posteo.net>
2022-01-16 13:51:28 -0500

    Utility functions for creating a property-based name of a struct or class

---
+++ code/utils/prop2str.m
@@ -0,0 +1,56 @@
+function [stringified_properties] = prop2str(obj, properties_to_skip, property_separator)
+    % Converts the property names and values of a struct or object
+    % into a character vector.
+    % For example, a struct, s, with the properties, s.a = 1, s.b = 2,
+    % would become 'a=1-b=2'.
+    % If some of the property values are cell arrays,
+    % they should be character vectors or numerical vectors
+    % and of the same type within each cell array.
+    % 
+    % Arguments:
+    %
+    %   obj: 1x1 struct or object
+    %       Object with properties to be stringified
+    % 
+    %   properties_to_skip: character vector or cell array
+    %       Properties to not include in the output character vector.
+    % 
+    % Returns:
+    % 
+    %   stringified_properties: character vector
+
+    stringified_properties = [];
+    props = properties(obj);
+
+    if nargin < 2
+        properties_to_skip = {};
+    elseif ischar(properties_to_skip)
+        properties_to_skip = {properties_to_skip};
+    end
+
+    if nargin < 3
+        property_separator = '-';
+    end
+
+    for ii = 1:length(props)-1
+        prop = props{ii};
+        val = obj.(prop);
+
+        % if property should be skipped, skip it
+        if any(strcmp(val, properties_to_skip))
+            continue
+        end
+        
+        % type checking and conversions
+        val = var2str(val);
+
+        string_chunk = [prop, '=', val];
+        stringified_properties = [stringified_properties, string_chunk, property_separator];
+    end
+
+    prop = props{end};
+    val = var2str(obj.(prop));
+    string_chunk = [prop, '=', val];
+    stringified_properties = [stringified_properties, string_chunk];
+
+end % function
---
+++ code/utils/var2str.m
@@ -0,0 +1,35 @@
+function [val] = var2str(val)
+    % Convert a numeric matrix, or cell array of numbers or character vectors
+    % into a character vector.
+
+
+    % type checking and conversions for val
+    % return a character vector with no whitespace
+    if isnumeric(val)
+        val = regexprep(num2str(val(:)'), '\s*', ',');
+        % is the data numeric?
+    elseif iscell(val)
+        if all(cellfun(@isnumeric, val))
+            % convert to character vector
+            val = num2str([val{:}]);
+            % replace whitespace with comma
+            val = regexprep(val(:)', '\s*', ',');
+        elseif all(cellfun(@ischar, val))
+            % combine all cells into a vector
+            % with commas between them
+            new_val = [];
+            for ii = 1:length(val)-1
+                new_val = [new_val, val{ii}(:)', ','];
+            end
+            val = [new_val, val{end}(:)'];
+        else
+            error('cell array doesn''t contain only numeric or characte vector data')
+        end
+    elseif ischar(val)
+        % assume everything is fine but replace whitespace
+        val = regexprep(val(:)', '\s*', ',');
+    else
+        error(['the class of this varible is: ', class(val), '. I don''t know what to do with this.'])
+    end
+
+end
