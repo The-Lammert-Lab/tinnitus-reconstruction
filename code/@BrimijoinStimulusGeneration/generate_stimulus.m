3cec2471ba30d5d4f93062fc5c34e665a082f996
Alec Hoyland <alec.hoyland@posteo.net>
2022-02-03 14:48:58 -0500

    Add stim gen functions for Brimijoin

---
+++ code/@BrimijoinStimulusGeneration/generate_stimulus.m
@@ -0,0 +1,26 @@
+function [stim, Fs, X, binned_repr] = generate_stimulus(self)
+    % Generate a matrix of stimuli
+    % where the matrix is of size nfft x n_trials.
+    % Bins are filled with an amplitude value chosen from self.amplitude_values
+    % with equal probability.
+    % 
+    % Class Properties Used:
+    %   n_bins
+    %   amplitude_values
+
+
+    [binnum, Fs, nfft] = self.get_freq_bins();
+
+    X = zeros(nfft/2, 1);
+    binned_repr = zeros(self.n_bins, 1);
+
+    for ii = 1:self.n_bins
+        this_amplitude_value = self.amplitude_values(randi(length(self.amplitude_values)));
+        binned_repr(ii) = this_amplitude_value;
+        X(binnum==ii) = this_amplitude_value;
+    end
+
+    % Synthesize Audio
+    stim = self.synthesize_audio(X, nfft);
+
+end
