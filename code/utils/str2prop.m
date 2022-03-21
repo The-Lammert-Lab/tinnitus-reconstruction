f956a410fd4a75bf992e6d4fede623edd1c4cae2
Alec Hoyland <alec.hoyland@posteo.net>
2022-03-21 17:22:14 -0400

    Add function to get structs back from property strings

---
+++ code/utils/str2prop.m
@@ -0,0 +1,47 @@
+function obj = str2prop(prop_string, properties_to_skip, property_separator, output_type)
+    % Converts a string of properties and values
+    % into a struct or cell array.
+    % TODO: more documentation, use property_separator
+    % 
+    % Arguments:
+    % 
+    %   prop_string: character vector
+    %       String containing property : value pairs
+    % 
+    %   properties_to_skip: character vector or cell array
+    %       Properties to not incude in the output character vector
+    % 
+    %   property_separator: character vector
+    %       What separator to use between parameter statements.
+    % 
+    % Returns:
+    % 
+    %   obj: struct or cell array
+    % 
+    % 
+    % Example:
+    % 
+    %   obj = str2prop(prop_string, [], '&&')
+    % 
+    % See Also: collect_parameters, str2prop
+    % 
+
+    arguments
+        prop_string {mustBeText}
+        properties_to_skip = []
+        property_separator = '&&'
+        output_type = 'struct'
+    end
+
+    regex_result = regexp(prop_string, '([\w_]+)=([\-\w\d\.\,]*)', 'tokens');
+    params_cell = cat(1, regex_result{:})';
+
+    if all(output_type ~= 'struct')
+        obj = params_cell;
+        return
+    end
+
+    obj = cell2struct(params_cell(2, :), params_cell(1, :), 2);
+
+
+end % function
