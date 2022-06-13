c7396f9ac636c47f166ed27421b88c9dd308fee3
Alec Hoyland <alec.hoyland@posteo.net>
2022-06-13 12:51:56 -0400

    Update Brimijoin stimulus generation function

--- code/stimulus_generation/@BrimijoinStimulusGeneration/generate_stimulus.m
+++ code/stimulus_generation/@BrimijoinStimulusGeneration/generate_stimulus.m
@@ -1,29 +1,44 @@
-function [stim, Fs, X, binned_repr] = generate_stimulus(self)
+function [stim, Fs, spect, binned_repr] = generate_stimulus(self)
     %
-    %   [stim, Fs, X, binned_repr] = generate_stimulus(self)
+    %   [stim, Fs, spect, binned_repr] = generate_stimulus(self)
+    % 
     % 
     % Generate a matrix of stimuli
-    % where the matrix is of size nfft x n_trials.
     % Bins are filled with an amplitude value chosen from self.amplitude_values
     % with equal probability.
+    % 
+    % Returns:
+    %   stim: n x 1 numerical vector
+    %       The stimulus waveform,
+    %       where n is self.nfft() + 1.
+    %   Fs: 1x1 numerical scalar
+    %       The sample rate in Hz.
+    %   spect: m x 1 numerical vector
+    %       The half-spectrum,
+    %       where m is self.nfft() / 2,
+    %       in dB.
+    %   binned_repr: self.n_bins x 1 numerical vector
+    %       The binned representation.
     % 
     % Class Properties Used:
     %   n_bins
     %   amplitude_values
+    % 
+    % See Also: get_freq_bins, generate_stimuli_matrix
 
 
     [binnum, Fs, nfft] = self.get_freq_bins();
 
-    X = zeros(nfft/2, 1);
+    % X = zeros(nfft/2, 1);
+    spect = self.get_empty_spectrum();
     binned_repr = zeros(self.n_bins, 1);
 
     for ii = 1:self.n_bins
         this_amplitude_value = self.amplitude_values(randi(length(self.amplitude_values)));
         binned_repr(ii) = this_amplitude_value;
-        X(binnum==ii) = this_amplitude_value;
+        spect(binnum==ii) = this_amplitude_value;
     end
 
     % Synthesize Audio
-    stim = self.synthesize_audio(X, nfft);
-
+    stim = self.synthesize_audio(spect, nfft);
 end
