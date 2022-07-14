5d6ede592a8fe7bcc6beb0cc466a39de7f87259d
Alec Hoyland <alec.hoyland@posteo.net>
2022-07-14 17:08:03 -0400

    Add reconstruction saving

--- code/scripts/pilot_reconstructions.m
+++ code/scripts/pilot_reconstructions.m
@@ -159,12 +159,25 @@ for ii = 1:length(numeric_columns)
     end
 end
 
-%% Visualization
+%% Saving Results
 
 T.reconstructions_cs_1 = reconstructions_cs(:, end);
 T.reconstructions_lr_1 = reconstructions_lr(:, end);
 T.reconstructions_rand = reconstructions_rand;
 T.reconstructions_synth = reconstructions_synth;
+
+% Save the reconstruction waveforms
+if PUBLISH
+    for ii = 1:height(T)
+        this_filepath = pathlib.join(DATA_DIR, [T.experiment_name{ii}, '.wav']);
+        this_spectrum = stimgen.binnedrepr2spect(T.reconstructions_cs_1{ii});
+        this_waveform = stimgen.synthesize_audio(this_spectrum, stimgen.get_nfft());
+        audiowrite(this_filepath, this_waveform, stimgen.Fs);
+    end
+end
+
+%% Visualization
+
 
 % Plotting the bin-representation of the target signal vs. the reconstructions
 
