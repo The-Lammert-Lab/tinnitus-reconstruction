075a269c49457ac894ef9418063fbeed587152cc
Alec Hoyland <alec.hoyland@posteo.net>
2022-08-31 14:55:08 -0400

    Allow the script to be run multiple times

--- code/scripts/munge_hashes.m
+++ code/scripts/munge_hashes.m
@@ -1,14 +1,31 @@
+%% munge_hashes.m 
+% Processes config files, correcting errors.
+% Then, fixes the hashes for saved data files
+% associated with changed config files.
+
+LEGACY = false;
+
+
 config_files = dir("config*.yaml");
 
 for ii = 1:length(config_files)
     config_file = config_files(ii).name;
-    config = parse_config(config_file, true, true);
+    config = parse_config(config_file, LEGACY, true);
     old_hash = get_hash(config);
     corelib.verb(true, 'munge_hashes', ['config is: ', config_file])
     corelib.verb(true, 'munge_hashes', ['hash is: ', old_hash])
 
     %% Get the new properties
-    target_signal_filepath = config.target_audio_filepath;
+
+    if isfield(config, 'target_audio_filepath')
+        target_signal_filepath = config.target_audio_filepath;
+        
+        % Update target signal filepath
+        config.target_signal_filepath = target_signal_filepath;
+        config = rmfield(config, 'target_audio_filepath');
+        corelib.verb(true, 'munge_hashes', 'removing target_audio_filepath, replacing with target_signal_filepath')
+    end
+
     if contains(config_file, 'buzz')
         target_signal_name = 'buzzing';
     elseif contains(config_file, 'roar')
@@ -20,8 +37,10 @@ for ii = 1:length(config_files)
     % Update properties
     config.target_signal_name = target_signal_name;
     corelib.verb(true, 'munge_hashes', ['new target signal name: ', target_signal_name])
-    config.target_signal_filepath = target_signal_filepath;
-    config = rmfield(config, 'target_audio_filepath');
+    
+    % Set stimuli save type
+    config.stimuli_save_type = 'bins';
+    corelib.verb(true, 'munge_hashes', 'fixing stimuli save type')
 
     % For 2-AFC
     if strcmp(config.experiment_name, 'paper1-buzzing-BWR') && isfield(config, 'two_afc') && config.two_afc
