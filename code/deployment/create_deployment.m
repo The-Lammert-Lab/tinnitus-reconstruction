1305e34a41d24ffe05b1865f9919bfe5b4ef69d0
Alec Hoyland <alec.hoyland@posteo.net>
2022-10-24 17:14:40 -0400

    Add more options

--- code/deployment/create_deployment.m
+++ code/deployment/create_deployment.m
@@ -5,6 +5,8 @@ function create_deployment(options)
     arguments
         options.verbose (1,1) {mustBeNumericOrLogical} = true
         options.version {mustBeText} = ''
+        options.standalone (1,1) {mustBeNumericOrLogical} = false
+        options.toolbox (1,1) {mustBeNumericOrLogical} = true
     end
 
     if isempty(options.version)
@@ -16,14 +18,17 @@ function create_deployment(options)
     disp(options)
 
     % Create the standalone
-    corelib.verb(options.verbose, 'INFO create_deployment', 'Creating the standalone...')
-    create_standalone(options.version);
-    corelib.verb(options.verbose, 'INFO create_deployment', 'Standalone created.')
-    
-    % Create the toolbox
-    corelib.verb(options.verbose, 'INFO create_deployment', 'Creating the toolbox...')
-    matlab.addons.toolbox.toolboxVersion('tinnitus-project.prj', options.version);
-    matlab.addons.toolbox.packageToolbox('tinnitus-project.prj', 'tinnitus-project.mltbx');
-    corelib.verb(options.verbose, 'INFO create_deployment', 'Toolbox created.')
+    if options.standalone
+        corelib.verb(options.verbose, 'INFO create_deployment', 'Creating the standalone...')
+        create_standalone(options.version);
+        corelib.verb(options.verbose, 'INFO create_deployment', 'Standalone created.')
+    end
 
+    % Create the toolbox
+    if options.toolbox
+        corelib.verb(options.verbose, 'INFO create_deployment', 'Creating the toolbox...')
+        matlab.addons.toolbox.toolboxVersion('tinnitus-project.prj', options.version);
+        matlab.addons.toolbox.packageToolbox('tinnitus-project.prj', 'tinnitus-project.mltbx');
+        corelib.verb(options.verbose, 'INFO create_deployment', 'Toolbox created.')
+    end
 end
