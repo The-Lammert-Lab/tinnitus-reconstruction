c82009ac2f6b269d229a950c644b0fcc6ecb5be2
Nelson Barnett <nelsonvbarnett@gmail.com>
2023-05-10 15:53:27 +1000

    Update subject_selection_process documentation.

--- code/utils/subject_selection_process.m
+++ code/utils/subject_selection_process.m
@@ -3,26 +3,39 @@
 % Returns a response vector and the stimuli
 % where the response vector is made of up -1 and 1 values
 % corresponding to yes and no statements
-% about how well the stimuli correspond to the target signal.
+% about how well the stimuli correspond to the representation.
 % 
 % ```matlab
-%   y = subject_selection_process(target_signal, stimuli)
-%   [y, X] = subject_selection_process(target_signal, [], n_samples)
+%   y = subject_selection_process(representation, stimuli)
+%   y = subject_selection_process(representation, stimuli, [], responses, 'mean_zero', true, 'response_thresh', 'noes')
+%   [y, X] = subject_selection_process(representation, [], n_samples)
 % ```
 % 
 % **ARGUMENTS:**
 % 
-%   - target_signal: `n x 1` numerical vector,
+%   - representation: `n x 1` numerical vector,
 %       the signal to compare against (e.g., the tinnitus signal).
 % 
 %   - stimuli: numerical matrix,
 %       an `m x n` matrix where m is the number of samples/trials
-%       and n is the same length as the target signal.
+%       and n is the same length as the representation.
 %       If stimuli is empty, a random Bernoulli matrix (p = 0.5) is used.
 %   
 %   - n_samples: integer scalar
 %       representing how many samples are used when generating the Bernoulli matrix default
 %       for stimuli, if the stimuli argument is empty.
+% 
+%   - responses: `m x 1` numerical vector, 
+%       which contains only `-1` and `1` values,
+%       used to determine the threshold if using one of the custom options.
+% 
+%   - options.mean_zero: `bool`, default: false, 
+%       representing a flag that centers the mean of the stimuli and representation.
+% 
+%   - options.response_thresh: `char`, either: `'yesses'` or `'noes'`, default: `''`,
+%       which determines by what measure the threshold 
+%       for choosing a "yes" response is determined. The default results in
+%       50% threshold. If using this option, `responses` must be passed as well.
 % 
 % **OUTPUTS:**
 % 
@@ -35,10 +48,10 @@
 % See Also: 
 % AbstractStimulusGenerationMethod.subject_selection_process
 
-function [y, X] = subject_selection_process(target_signal, stimuli, n_samples, responses, options)
+function [y, X] = subject_selection_process(representation, stimuli, n_samples, responses, options)
     
     arguments
-        target_signal (:,1) {mustBeNumeric}
+        representation (:,1) {mustBeNumeric}
         stimuli (:,:) {mustBeNumeric, mustBeReal}
         n_samples {mustBeNumeric, mustBePositive, mustBeInteger} = []
         responses (:,1) {mustBeNumeric} = []
@@ -47,16 +60,16 @@ function [y, X] = subject_selection_process(target_signal, stimuli, n_samples, r
     end
 
     if isempty(stimuli)
-        X = round(rand(n_samples, length(target_signal)));
+        X = round(rand(n_samples, length(representation)));
     else
         X = stimuli;
     end
 
     % Projection
     if options.mean_zero
-        e = (X - mean(X,2)) * (target_signal(:) - mean(target_signal(:)));
+        e = (X - mean(X,2)) * (representation(:) - mean(representation(:)));
     else
-        e = X * target_signal(:);
+        e = X * representation(:);
     end
 
     % Percentile is percent of "yes" or "no" answers if specified.
--- docs/utils.md
+++ docs/utils.md
@@ -717,26 +717,39 @@ obj = str2prop(prop_string, [], '&&')
 Returns a response vector and the stimuli
 where the response vector is made of up -1 and 1 values
 corresponding to yes and no statements
-about how well the stimuli correspond to the target signal.
+about how well the stimuli correspond to the representation.
 
 ```matlab
-y = subject_selection_process(target_signal, stimuli)
-[y, X] = subject_selection_process(target_signal, [], n_samples)
+y = subject_selection_process(representation, stimuli)
+y = subject_selection_process(representation, stimuli, [], responses, 'mean_zero', true, 'response_thresh', 'noes')
+[y, X] = subject_selection_process(representation, [], n_samples)
 ```
 
 **ARGUMENTS:**
 
-- target_signal: `n x 1` numerical vector,
+- representation: `n x 1` numerical vector,
 the signal to compare against (e.g., the tinnitus signal).
 
 - stimuli: numerical matrix,
 an `m x n` matrix where m is the number of samples/trials
-and n is the same length as the target signal.
+and n is the same length as the representation.
 If stimuli is empty, a random Bernoulli matrix (p = 0.5) is used.
 
 - n_samples: integer scalar
 representing how many samples are used when generating the Bernoulli matrix default
 for stimuli, if the stimuli argument is empty.
+
+- responses: `m x 1` numerical vector, 
+which contains only `-1` and `1` values,
+used to determine the threshold if using one of the custom options.
+
+- options.mean_zero: `bool`, default: false, 
+representing a flag that centers the mean of the stimuli and representation.
+
+- options.response_thresh: `char`, either: `'yesses'` or `'noes'`, default: `''`,
+which determines by what measure the threshold 
+for choosing a "yes" response is determined. The default results in
+50% threshold. If using this option, `responses` must be passed as well.
 
 **OUTPUTS:**
 
