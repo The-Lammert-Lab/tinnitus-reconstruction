06139c1f0c5108e499ef4f5580dae9469ff26855
Nelson Barnett <nelsonvbarnett@gmail.com>
2023-08-28 11:16:05 -0400

    Process survey responses

---
+++ code/scripts/survey_viz.m
@@ -0,0 +1,58 @@
+%%%
+% This script collects and analyzes subjective rankings from
+% compare_recons.m
+%%%
+
+%% Data setup
+data_dir = '~/Desktop/tmp';
+d = dir(pathlib.join(data_dir, 'survey_*.csv'));
+
+%% Load
+fname = d(1).name;
+underscores = find(fname == '_');
+user_id = fname(underscores(end)+1:end-4);
+T = readtable(fullfile(d(1).folder, d(1).name));
+T.user_id = {user_id};
+
+for ii = 2:length(d)
+    fname = d(ii).name;
+    underscores = find(fname == '_');
+    user_id = fname(underscores(end)+1:end-4);
+    T2 = readtable(fullfile(d(ii).folder, d(ii).name));
+    T2.user_id = {user_id};
+    T = [T; T2];
+end
+
+%% Plot setup
+user_ids = unique(T.user_id);
+unique_hashes = unique(T.hash);
+lbl = {'white noise', 'standard', 'sharpened'};
+
+%% Bar ratings
+figure
+tiledlayout('flow')
+for ii = 1:length(unique_hashes)
+    this_hash = unique_hashes(ii);
+    ind = strcmp(T.hash,this_hash);
+    y = [T.whitenoise(ind, :), ...
+        T.recon_standard(ind, :), ...
+        T.recon_adjusted(ind, :)]';
+
+    nexttile
+    bar(y)
+    set(gca, 'XTickLabel', lbl, 'XTick', 1:length(lbl), 'YTick', 1:7)
+    title(this_hash, 'FontSize', 16)
+    grid on
+    legend(user_ids)
+end
+
+%% Scatter params
+figure
+for ii = 1:length(user_ids)
+    ind = strcmp(T.user_id,user_ids(ii));
+    scatter(T.mult(ind,:), T.binrange(ind,:), 'filled');
+    hold on
+end
+legend(user_ids)
+xlabel('Mult param', 'FontSize', 16)
+ylabel('Binrange param', 'FontSize', 16)
