709c25817919027674b0dade5198ea1253c9ffed
Nelson Barnett <nelsonvbarnett@gmail.com>
2023-11-28 16:35:25 -0500

    Add docs to pure_tone

--- code/utils/pure_tone.m
+++ code/utils/pure_tone.m
@@ -1,3 +1,19 @@
+% ### pure_tone
+%
+% Generate a sinusoidal pure tone stimuli
+% 
+% **ARGUMENTS:**
+% 
+% tone_freq: `1 x 1` positive scalar, the frequency to play
+% dur: `1 x 1` positive scalar, 
+%   the duration of the sound in seconds, default: 0.5  
+% Fs: `1 x 1` positive scalar, 
+%   the sample rate of the sound in Hz, deafult: 44100
+% 
+% **OUTPUTS:**
+% 
+%  stim: `1 x n` numerical vector, the sinusoidal waveform
+
 function stim = pure_tone(tone_freq,dur,Fs)
     arguments
         tone_freq (1,1) {mustBePositive, mustBeReal}
@@ -5,5 +21,5 @@ function stim = pure_tone(tone_freq,dur,Fs)
         Fs (1,1) {mustBePositive, mustBeReal} = 44100
     end
     t = 0:1/Fs:dur;
-    stim = sin(2*pi*tone_freq.*t);
+    stim = sin(2*pi*tone_freq.*t)';
 end
