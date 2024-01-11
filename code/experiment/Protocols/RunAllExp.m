f25d71a7039f792102c2e1a54d357b81e7877045
Nelson Barnett <nelsonvbarnett@gmail.com>
2024-01-11 11:38:01 -0500

    Add argument for how many times to run pitch match, update RC break screen

--- code/experiment/Protocols/RunAllExp.m
+++ code/experiment/Protocols/RunAllExp.m
@@ -2,10 +2,11 @@
 % This function goes one by one through each of the experimental protocols
 % using one config file.
 
-function RunAllExp(cal_dB, config_path)
+function RunAllExp(cal_dB, config_path, n_pm)
     arguments
         cal_dB (1,1) {mustBeReal}
         config_path (1,:) char = ''
+        n_pm (1,1) {mustBePositive, mustBeInteger} = 3
     end
 
     if isempty(config_path)
@@ -15,6 +16,10 @@ function RunAllExp(cal_dB, config_path)
 
     ThresholdDetermination(cal_dB,'config_file',config_path,'del_fig',false);
     LoudnessMatch(cal_dB,'config_file',config_path,'fig',gcf,'del_fig',false);
-    PitchMatch(cal_dB,'config_file',config_path,'fig',gcf,'del_fig',false);
+
+    for ii = 1:n_pm
+        PitchMatch(cal_dB,'config_file',config_path,'fig',gcf,'del_fig',false);
+    end
+     
     RevCorr('config_file',config_path,'fig',gcf);
 end
